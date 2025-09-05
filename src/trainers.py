d46ac26366e3d8ee812c9c8b47e52679cf7a884b
孙悟空是也 <64391665+Sun-Sir@users.noreply.github.com>
2025-09-05 15:50:38 +0800

    Add optional popularity encoding and fix trainer compatibility

--- src/trainers.py
+++ src/trainers.py
@@ -106,9 +106,20 @@ class Trainer:
 
 
                 user_ids, input_ids, time1_seq, time2_seq, answers, neg_answer, same_target = batch
-                loss = self.model.calculate_loss(
-                    input_ids, answers, neg_answer, same_target, user_ids, time1_seq, time2_seq
-                )
+                try:
+                    loss = self.model.calculate_loss(
+                        input_ids,
+                        answers,
+                        neg_answer,
+                        same_target,
+                        user_ids,
+                        time1_seq,
+                        time2_seq,
+                    )
+                except TypeError:
+                    loss = self.model.calculate_loss(
+                        input_ids, answers, neg_answer, same_target, user_ids
+                    )
 
                     
                 self.optim.zero_grad()
@@ -133,7 +144,12 @@ class Trainer:
                 batch = tuple(t.to(self.device) for t in batch)
 
                 user_ids, input_ids, time1_seq, time2_seq, answers, _, _ = batch
-                recommend_output = self.model.predict(input_ids, user_ids, time1_seq, time2_seq)
+                try:
+                    recommend_output = self.model.predict(
+                        input_ids, user_ids, time1_seq, time2_seq
+                    )
+                except TypeError:
+                    recommend_output = self.model.predict(input_ids, user_ids)
 
                 recommend_output = recommend_output[:, -1, :]# 推荐的结果
                 
